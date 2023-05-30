e1115afc14f4c9420772f638f6313fe1c527d661
Mohamed Hosam <medo.hosam1@yahoo.com>
2023-05-30 14:44:56 +0300

    navbar almost finsihed
    small adjustment to the navbar needed to fix the spacing

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar/navbar.component';
-import {MatTabsModule} from '@angular/material/tabs';
-import {MatCardModule} from '@angular/material/card';
 import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatIconModule} from '@angular/material/icon';
-
-import {MatDividerModule} from '@angular/material/divider';
 import {MatButtonModule} from '@angular/material/button';
 
 
@@ -16,11 +11,7 @@ import {MatButtonModule} from '@angular/material/button';
   ],
   imports: [
     CommonModule,
-    MatTabsModule,
-    MatCardModule,
     MatToolbarModule,
-    MatIconModule,
-    MatDividerModule,
     MatButtonModule
   ],
   exports:[
